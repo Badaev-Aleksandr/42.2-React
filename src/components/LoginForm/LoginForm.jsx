2c2b001b740f0d4c3bf80f9be6a3680b19eaffb2
Aleksandr <badaev92aleksandr@gmail.com>
2024-11-14 19:29:15 +0100

    Add correction homework03

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,10 +1,6 @@
 import Button from "../Button/Button";
 import Input from "../Input/Input";
 import "./styles.css";
-<link
-  href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
-  rel="stylesheet"
-></link>;
 
 function LoginForm() {
   return (
